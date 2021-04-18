ac01ab273e42523c87ee0ebfbc30c1b8a2bff00b
RimpyGoyal <rimpy.goyal11@gmail.com>
2021-04-17 21:21:09 -0500

    Shapes with Xml

---
+++ Shapes-List/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes_List
+{
+
+    [Serializable]
+    public class Circle : Shape
+    {
+        public double Radius { set; get; }
+
+        public override string Color { get => base.Color; set => base.Color = value; }
+        public override double Area => Math.PI * (Radius * Radius);
+        public override string Name => GetType().Name;
+    }
+
+}
---
+++ Shapes-Xml/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+using Shapes_List;
+
+namespace Shapes_Xml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<Shape> ListOfShapes = new List<Shape>
+           
+            {
+                new Circle { Color = "Red", Radius = 2.5 },
+                new Rectangle { Color = "Blue", Height = 20.0, Width = 10.0 },
+                new Circle { Color = "Green", Radius = 8 },
+                new Circle { Color = "Purple", Radius = 12.3 },
+                new Rectangle { Color = "Blue", Height = 45.0, Width = 18.0 }
+            };
+
+            ToXml(ListOfShapes, "abc.xml");
+
+            List<Shape> loadedShapesXml = FromXml<List<Shape>>("listOfShapes.xml");
+
+            Console.WriteLine("Loading shapes from XML:\n");
+
+            foreach (var item in loadedShapesXml)
+            {
+                Console.WriteLine($"{item.Name} is {item.Color} and has an area of {item.Area}");
+            }
+
+            Console.WriteLine("\nHave a nice day!");
+        }
+        public static void ToXml<T>(T obj, string path)
+        {
+            using (StringWriter sw = new StringWriter(new StringBuilder()))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+                xmlSerializer.Serialize(sw, obj);
+                File.WriteAllText(path, sw.ToString());
+            }
+        }
+
+        public static T FromXml<T>(string path)
+        {
+            string xmlString = File.ReadAllText(path);
+            using (StringReader sr = new StringReader(xmlString))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+                return (T)xmlSerializer.Deserialize(sr);
+            }
+        }
+
+   }
+}
+
